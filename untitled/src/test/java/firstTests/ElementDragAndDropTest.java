a676e3ffd22ea493b500f0e9376115983d1fe89e
matashkov-de <mat.dmitry539@gmail.com>
2022-02-22 17:09:14 +0500

    junit 5example test added

---
+++ untitled/src/test/java/firstTests/ElementDragAndDropTest.java
@@ -0,0 +1,23 @@
+package firstTests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ElementDragAndDropTest {
+    @Test
+    void dragAndDropElementSwapPlaces() {
+        //Открыть страницу с элементами
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        //Перенесите прямоугольник А на место В
+        $("#column-a").dragAndDropTo($("#column-b"));
+
+
+        //check: Элементы  поменялись местами
+        $("#column-a header").shouldHave(text("B"));
+        $("#column-b header").shouldHave(text("A"));
+        System.out.println("Элементы поменялись местами");
+    }
+}
