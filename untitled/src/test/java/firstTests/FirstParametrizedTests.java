99d6b2e0ad66b7cba87f5bda85d42894f18273c0
matashkov-de <mat.dmitry539@gmail.com>
2022-02-28 17:38:48 +0500

    CSVSourses annotation added

--- untitled/src/test/java/firstTests/FirstParametrizedTests.java
+++ untitled/src/test/java/firstTests/FirstParametrizedTests.java
@@ -1,20 +1,15 @@
 package firstTests;
 
-
-
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
@@ -37,6 +32,7 @@ public class FirstParametrizedTests {
         Selenide.closeWebDriver();
     }
 
+
     @ValueSource (strings = {"English", "Русского"})
     @ParameterizedTest (name = "Отображение {0} текста в отправленной форме")
     void textVisibleInOutputForm (String testData) {
@@ -47,22 +43,23 @@ public class FirstParametrizedTests {
         $(byText("Submit")).click();
 
         //check: В отправленной форме отображается введенное имя
-        $$("#name").find(text(testData)).shouldBe(visible);
+        $$("#output").find(text(testData)).shouldBe(visible);
     }
 
+
     @CsvSource ({
-            "Букв, МоеИмя",
-            "Цифр, 123"
+            "text, МоеИмя",
+            "email, name@example.com"
     })
-    @ParameterizedTest (name = "Отображение {0}  в отправленной форме")
+    @ParameterizedTest (name = "Отображение типа текста \"{0}\" в отправленной форме")
     void languageTextVisibleInOutputForm (String testData, String visibleResult) {
         //Ввести полное имя
-        $("#userName").setValue(testData);
+        $$("input").findBy(type(testData)).setValue(visibleResult);
 
         //Нажать кнопку отправки формы
         $(byText("Submit")).click();
 
         //check: В отправленной форме отображается имя на нужном языке
-        $$("#name").find(text(visibleResult)).shouldBe(visible);
+        $$("#output").find(text(visibleResult)).shouldBe(visible);
     }
 }
